f3bded8a3643308ad42df8913fb94cbcf7e26ada
Vasil <v.mitov.clouway@gmail.com>
2016-10-24 11:37:17 +0300

    Changes to connection provider.

--- src/com/clouway/core/Provider.java
+++ src/com/clouway/core/Provider.java
@@ -1,10 +1,10 @@
 package com.clouway.core;
 
-import java.sql.SQLException;
+
 
 /**
  * @author Vasil Mitov <v.mitov.clouway@gmail.com>
  */
 public interface Provider<T> {
-  T get() throws SQLException;
+  T get();
 }
