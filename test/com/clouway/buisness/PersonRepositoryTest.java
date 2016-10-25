e301aa106c8fda76cefed68dfffcefc2042f8958
Vasil <v.mitov.clouway@gmail.com>
2016-10-25 14:29:26 +0300

    Changes for the tests to be more readable.

--- test/com/clouway/buisness/PersonRepositoryTest.java
+++ test/com/clouway/buisness/PersonRepositoryTest.java
@@ -21,65 +21,62 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class PersonRepositoryTest {
   private ConnectionProvider connectionProvider = new ConnectionProvider("TASK2");
   private PersistentPersonRepository personRepository = new PersistentPersonRepository(connectionProvider);
-
-  private TableManagerForTest tableManager=new TableManagerForTest(connectionProvider);
-  private CalendarForTest calendar=new CalendarForTest();
-  private Person person1 = new Person("Vasko", "1111111111", 25, "pojoemail@object.com");
-  private Person person2 = new Person("Vaskis", "1111111112", 23, "pojoemail@object.com");
-  private Person person3 = new Person("Marto", "1111111113", 28, "pojoemail@object.com");
-
+  private TableManagerForTest tableManager = new TableManagerForTest(connectionProvider);
+  private CalendarForTest calendar = new CalendarForTest();
 
   @Before
-  public void setup(){
+  public void setup() {
     tableManager.deleteTable("TRIP");
     tableManager.deleteTable("PEOPLE");
-    personRepository.register(person1);
-    personRepository.register(person2);
-    personRepository.register(person3);
   }
 
 
   @Test
   public void happyPath() throws Exception {
-    Person expected=new Person("Vasko", "1111111111", 25, "pojoemail@object.com");
-    Optional<Person> actual=personRepository.find("1111111111");
-    assertThat(actual.get(),is(expected));
+    personRepository.register(new Person("Vasko", "1111111111", 25, "pojoemail@object.com"));
+    Person expected = new Person("Vasko", "1111111111", 25, "pojoemail@object.com");
+    Optional<Person> actual = personRepository.find("1111111111");
+    assertThat(actual.get(), is(expected));
   }
 
   @Test
   public void notFindingAPerson() throws Exception {
-    Optional<Person> expected=Optional.empty();
-    Optional<Person> actual=personRepository.find("2111111111");
-    assertThat(actual,is(expected));
+    Optional<Person> expected = Optional.empty();
+    Optional<Person> actual = personRepository.find("2111111111");
+    assertThat(actual, is(expected));
   }
-
 
 
   @Test
   public void findAllStartingWithSequens() throws Exception {
-    List<Person> expected=new LinkedList<>();
+    personRepository.register(new Person("Vasko", "1111111111", 25, "pojoemail@object.com"));
+    personRepository.register(new Person("Vaskis", "1111111112", 23, "pojoemail@object.com"));
+    personRepository.register(new Person("Marto", "1111111113", 28, "pojoemail@object.com"));
+    List<Person> expected = new LinkedList<>();
     expected.add(new Person("Vasko", "1111111111", 25, "pojoemail@object.com"));
     expected.add(new Person("Vaskis", "1111111112", 23, "pojoemail@object.com"));
-    List<Person> actual=personRepository.findAllStartingWith("V");
-    assertThat(actual,is(expected));
+    List<Person> actual = personRepository.findAllStartingWith("V");
+    assertThat(actual, is(expected));
   }
 
   @Test
   public void visitingAtTheSameTime() throws Exception {
+    personRepository.register(new Person("Vasko", "1111111111", 25, "pojoemail@object.com"));
+    personRepository.register(new Person("Vaskis", "1111111112", 23, "pojoemail@object.com"));
+    personRepository.register(new Person("Marto", "1111111113", 28, "pojoemail@object.com"));
 
-    PersistentTripRepository tripRepository=new PersistentTripRepository(connectionProvider);
+    PersistentTripRepository tripRepository = new PersistentTripRepository(connectionProvider);
     Trip tripToPleven = new Trip("1111111111", calendar.getDate(10, 12, 2016), calendar.getDate(12, 12, 2016), "Pleven");
     Trip tripToPleven2 = new Trip("1111111112", calendar.getDate(10, 12, 2016), calendar.getDate(12, 12, 2016), "Pleven");
     tripRepository.register(tripToPleven);
     tripRepository.register(tripToPleven2);
 
-    List<Person> expected=new LinkedList<>();
+    List<Person> expected = new LinkedList<>();
     expected.add(new Person("Vasko", "1111111111", 25, "pojoemail@object.com"));
     expected.add(new Person("Vaskis", "1111111112", 23, "pojoemail@object.com"));
 
-    List<Person> actual=personRepository.findAllStayingAtTheSameTime("Pleven",calendar.getDate(11,12,2016));
+    List<Person> actual = personRepository.findAllStayingAtTheSameTime("Pleven", calendar.getDate(11, 12, 2016));
 
-    assertThat(actual,is(expected));
-
+    assertThat(actual, is(expected));
   }
 }
