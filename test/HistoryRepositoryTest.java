84a238de171970069201da302c66543c994b73e6
Vasil <v.mitov.clouway@gmail.com>
2016-10-26 11:32:20 +0300

    Removed unnessesary lines.

--- test/HistoryRepositoryTest.java
+++ test/HistoryRepositoryTest.java
@@ -17,7 +17,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * @author Vasil Mitov <v.mitov.clouway@gmail.com>
  */
-public class DisplayingHistoryTest {
+public class HistoryRepositoryTest {
   private ConnectionProvider connectionProvider = new ConnectionProvider("TASK3");
   private PersistentStockRepository stockRepository = new PersistentStockRepository(connectionProvider);
   private PersistentStockHistoryRepository historyRepository = new PersistentStockHistoryRepository(connectionProvider);
@@ -30,14 +30,10 @@ public class DisplayingHistoryTest {
     Stock pear = new Stock("pear", 2.2, 4.6);
     Stock orange = new Stock("orange", 3.1, 5.1);
     Stock potato = new Stock("potato", 2.3, 6.5);
-
-
     stockRepository.register(apple);
     stockRepository.register(pear);
     stockRepository.register(orange);
     stockRepository.register(potato);
-
-
     stockRepository.updateQuantity("apple", 1.1);
     stockRepository.updateQuantity("pear", 1.2);
     stockRepository.updateQuantity("orange", 4.2);
@@ -48,13 +44,10 @@ public class DisplayingHistoryTest {
   public void displayFullHistory() throws Exception {
     List<Stock> actual = historyRepository.getAll();
     List<Stock> expected = new LinkedList<>();
-
     expected.add(new Stock("apple", 1.2, 3.5));
     expected.add(new Stock("pear", 2.2, 4.6));
     expected.add(new Stock("orange", 3.1, 5.1));
     expected.add(new Stock("potato", 2.3, 6.5));
-
-
     assertThat(actual, is(expected));
   }
 
@@ -62,10 +55,8 @@ public class DisplayingHistoryTest {
   public void displayInPages() throws Exception {
     List<Stock> actual = historyRepository.getPages(2, 2);
     List<Stock> expected = new LinkedList<>();
-
     expected.add(new Stock("orange", 3.1, 5.1));
     expected.add(new Stock("potato", 2.3, 6.5));
-
     assertThat(actual, is(expected));
   }
 
@@ -73,9 +64,7 @@ public class DisplayingHistoryTest {
   public void displayLastPageWithoutHavingenoughtStockToFillIt() throws Exception {
     List<Stock> actual = historyRepository.getPages(3, 1);
     List<Stock> expected = new LinkedList<>();
-
     expected.add(new Stock("potato", 2.3, 6.5));
-
     assertThat(actual, is(expected));
   }
 
